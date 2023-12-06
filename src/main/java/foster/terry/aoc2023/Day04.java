05c789ee0de3855c909b6226c5157b11dbb7dab9
terrywfoster <terryfoster@yahoo.com>
2023-12-06 09:42:16 -0500

    Day04 My Improvements

--- src/main/java/foster/terry/aoc2023/Day04.java
+++ src/main/java/foster/terry/aoc2023/Day04.java
@@ -6,29 +6,32 @@ import java.util.stream.IntStream;
 
 public class Day04 {
 
-    private final List<Set<Integer>> winningNumberSet = new ArrayList<>();
-    private final List<Set<Integer>> myNumberSet = new ArrayList<>();
+
+    private final List<Set<Integer>> gameMatches = new ArrayList<>();
 
     public Day04(final List<String> input) {
         for(String card : input) {
-            Set<Integer> winningNumbers = Arrays.stream(card.substring(card.indexOf(":")+2, card.indexOf("|")-1).trim().replace("  "," ").split(" ")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet());
-            Set<Integer> myNumbers = Arrays.stream(card.substring(card.indexOf("|")+2).trim().replace("  "," ").split(" ")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet());
-            winningNumberSet.add(winningNumbers);
-            myNumberSet.add(myNumbers);
+            Set<Integer> winningNumbers = Arrays.stream(card.substring(card.indexOf(":")+1, card.indexOf("|")-1).trim().split(" +"))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toSet());
+            Set<Integer> myNumbers = Arrays.stream(card.substring(card.indexOf("|")+1).trim().split(" +"))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toSet());
+
+            winningNumbers.retainAll(myNumbers);
+            gameMatches.add(winningNumbers);
         }
     }
 
     public int scratchCardTotalPoints() {
         int totalPoints = 0;
-        for (int z = 0; z < winningNumberSet.size(); z++) {
+        for (Set<Integer> game : gameMatches) {
             int cardPoints = 0;
-            Set<Integer> winningNumbers = winningNumberSet.get(z);
-            Set<Integer> myNumbers = myNumberSet.get(z);
-
-            winningNumbers.retainAll(myNumbers);
-            if (!winningNumbers.isEmpty()) {
+            if (!game.isEmpty()) {
                 cardPoints = 1;
-                for (int x = 1; x < winningNumbers.size(); x++) {
+                for (int x = 1; x < game.size(); x++) {
                     cardPoints *= 2;
                 }
             }
@@ -41,22 +44,18 @@ public class Day04 {
     }
 
     public int totalScratchCards() {
-        final int[] cardCounts = new int[winningNumberSet.size()];
-
-        for(int x = 0; x < winningNumberSet.size(); x++) {
-            Set<Integer> winningNumbers = winningNumberSet.get(x);
-            Set<Integer> myNumbers = myNumberSet.get(x);
-            winningNumbers.retainAll(myNumbers);
-
-            int wonCards = winningNumbers.size();
+        final int[] cardCounts = new int[gameMatches.size()];
+        for(int x = 0; x < gameMatches.size(); x++) {
+            int wonCards = gameMatches.get(x).size();
             int currentCardCount = cardCounts[x] + 1;
             for (int c = 0; c < currentCardCount; c++) {
                 for (int w = 0; w < wonCards; w++) {
                     cardCounts[x+1+w]++;
                 }
             }
+
         }
 
-        return IntStream.of(cardCounts).sum() + winningNumberSet.size();
+        return IntStream.of(cardCounts).sum() + gameMatches.size();
     }
 }
